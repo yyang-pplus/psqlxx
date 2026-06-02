533fb290930d5af8b1105c735bf17a90cd2bf118
Yang Yang <yyang.pplus@gmail.com>
2026-06-01 21:51:35 -0700

    3rdParty: Upgrade to cxxopts v3.3.1

--- psqlxx/args.cpp
+++ psqlxx/args.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <psqlxx/args.hpp>
 #include <psqlxx/version.hpp>
 
@@ -7,10 +9,7 @@ namespace psqlxx {
 cxxopts::Options CreateBaseOptions() {
     cxxopts::Options options("psqlxx", "psql in C++.");
 
-    options.add_options()
-    ("h,help", "print usage")
-    ("V,version", "print version")
-    ;
+    options.add_options()("h,help", "print usage")("V,version", "print version");
 
     return options;
 }
@@ -19,9 +18,9 @@ std::optional<cxxopts::ParseResult>
 ParseOptions(cxxopts::Options &options, int argc, char **argv) noexcept {
     try {
         return options.parse(argc, argv);
-    } catch (const cxxopts::option_not_exists_exception &e) {
+    } catch (const cxxopts::exceptions::no_such_option &e) {
         std::cout << "Unrecognised command-line options: " << e.what() << std::endl;
-    } catch (const cxxopts::OptionException &e) {
+    } catch (const cxxopts::exceptions::exception &e) {
         std::cerr << "Error parsing command-line options: " << e.what() << std::endl;
     }
 
@@ -42,4 +41,4 @@ void HandleBaseOptions(const cxxopts::Options &options,
     }
 }
 
-}//namespace psqlxx
+} //namespace psqlxx
